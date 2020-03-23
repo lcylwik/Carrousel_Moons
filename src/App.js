a68220c1f3253364db62d4a559430f70daf8162f
lcylwik <cylwiklianet@gmail.com>
2020-03-22 21:25:00 -0600

    delete test carousel

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Elements from './carrousel/elements'
-// import Carrousel from './carrousel_v2/carrousel'
 
 function App() {
   return (
     <div className="App">
       <Elements></Elements> 
-      {/* <Carrousel></Carrousel> */}
     </div>
   );
 }
