6369d9be8a50289537f546e3c2b375ce9125b6c0
lcylwik <cylwiklianet@gmail.com>
2020-03-24 18:17:11 -0600

    module css desktop

--- src/carrousel/components/desktop/desktop.jsx
+++ src/carrousel/components/desktop/desktop.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../../style/index.css';
+import style from './desktop.module.css'
 import Description from '../description/description';
 import LinkCita from '../link/link';
 
@@ -13,12 +13,12 @@ class Desktop extends React.Component {
 
     render() {
         return (
-            <div className="carousel_desktop">
+            <div className={style.CarouselDesktop}>
                 {this.info.map((info, index) => {
                     const image = require(`../../assets/${index + 1}/rectangle.jpg`);
                     return (
-                        <div key={index} className="photo_desktop" >
-                            <img src={image} alt={index} className="carousel_image" />
+                        <div key={index} className={style.PhotoDesktop}>
+                            <img src={image} alt={index} className={style.CarouselImage} />
                             <Description key={index} item={info}></Description>
                             {index === 0 && <LinkCita></LinkCita>}
                         </div>
