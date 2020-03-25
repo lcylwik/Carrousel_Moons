b1dde8549f5f3974ddc950ec331c8bceb20b7398
lcylwik <cylwiklianet@gmail.com>
2020-03-25 12:22:43 -0600

    component description

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import CarrouselSteps from './carrousel/components/index'
+import CarrouselSteps from './carrousel/components/index';
+import CarrouselTestimonials from './carrousel_testimonials/components/index'
 
 function App() {
   return (
     <div className="App">
       <CarrouselSteps></CarrouselSteps> 
+      <CarrouselTestimonials></CarrouselTestimonials>
     </div>
   );
 }
---
+++ src/carrousel_testimonials/components/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import style  from './index.module.css'
+import Desktop from './desktop/desktop';
+import Mobile from './mobile/mobile';
+import Tablet from './tablet/tablet';
+import { data } from '../data/testimonials.json'
+
+const CarrouselTestimonials = () => {
+
+  const information = data;
+  const textHeader = information[1].header;
+  const createMarkup = () => { return { __html: textHeader }; };
+
+  return (
+    <div className={style.CarouselContainer}>
+      <div className={style.CarouselHeader}>
+        <p className={style.HeaderTitle} dangerouslySetInnerHTML={createMarkup()}></p>
+      </div>
+     <Mobile info={information} />
+       {/* <Tablet info={information} />
+      <Desktop info={information}/> */}
+    </div>
+  );
+}
+
+export default CarrouselTestimonials;
