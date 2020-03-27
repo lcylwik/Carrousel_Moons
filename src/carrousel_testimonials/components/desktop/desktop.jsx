f53e0c21a1c1caf99125c94e08c9601974ae8591
lcylwik <cylwiklianet@gmail.com>
2020-03-26 21:26:44 -0600

    carrousel desktop

--- src/carrousel_testimonials/components/desktop/desktop.jsx
+++ src/carrousel_testimonials/components/desktop/desktop.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { createRef } from 'react';
 import style from './desktop.module.css'
 import Description from '../description/description';
 import { loadedImgTable, dinamicRef } from '../../utils';
@@ -8,33 +8,150 @@ class Desktop extends React.Component {
     constructor(props) {
         super(props);
 
-        this.info = this.props.info;
-        this.refSlides = dinamicRef(this.info.length);
+        this.info = this.props.info
+
+        this.curLeft = 0
+        this.moveX = 0
+        this.startX = 0
+        this.curSlide = 0
+        this.loadedCnt = 0;
+        this.slideW = 0;
+        this.slideMargin = 0;
+        this.totalSlides = this.info.length;
+
+        this.refSliderDesktop = createRef();
+        this.refSlides = dinamicRef(this.totalSlides);
+
+        this.def = {
+            transition: {
+                speed: 300,
+                easing: ''
+            },
+            swipe: true,
+            autoHeight: false,
+        }
+    }
+
+    componentWillMount() {
+        window.addEventListener('resize', this.updateSliderDimension);
+    }
+
+    componentWillUnmount() {
+        window.addEventListener('resize', this.updateSliderDimension);
     }
 
     componentDidMount() {
-        for (let item of this.refSlides) {
-            loadedImgTable(item.current, ()=>{}, this.info.length, false);
+        this.init();
+        this.updateSliderDimension();
+    }
+
+    init = () => {
+        let allSlide = this.refSlides;
+
+        for (let item of allSlide) {
+            loadedImgTable(item.current, this.updateSliderDimension, this.totalSlides);
         }
+        this.getSlideW();
+    }
+
+    updateSliderDimension = (e) => {
+        this.refSliderDesktop.current.style.width = `${(450 + this.slideMargin) * this.totalSlides}px`;
+
+        this.slideW = this.getSlideW();
+        this.refSliderDesktop.current.style.left = `${- this.slideW * this.curSlide}px`;
+    }
+
+    getSlideW = () => {
+        const allSlider = this.refSlides;
+        let node = allSlider[0].current;
+        if (allSlider.length > 0 && node) {
+            this.slideW = parseInt(node.offsetWidth);
+            let nodeStyle = window.getComputedStyle(node)
+            this.slideMargin = parseInt(nodeStyle.getPropertyValue('margin-right'));
+        } else {
+            this.slideW = 0;
+        }
+        return this.slideW;
+    }
+
+    gotoSlide = (n) => {
+        debugger
+        if (n === "prev" && this.curSlide !== 0) this.curSlide--;
+        if (n === "next" && this.curSlide !== this.totalSlides - 1) this.curSlide++
+
+        this.refSliderDesktop.current.style.transition = `left ${this.def.transition.speed / 1000}s ${this.def.transition.easing}`;
+        this.refSliderDesktop.current.style.left = `${-this.curSlide * (this.slideW + this.slideMargin * 2)}px`
+
+        setTimeout(() => {
+            this.refSliderDesktop.current.style.transition = ''
+        }, this.def.transition.speed);
+    }
+
+    getCurrentLeft = () => {
+        const left = this.refSliderDesktop.current.style.left
+        if (left) this.curLeft = parseInt(left, 10);
+    }
+
+    startMove = (e) => {
+        this.getCurrentLeft();
+        const touch = e.targetTouches[0] || e.changedTouches[0];
+        this.startX = touch.pageX;
+    }
+
+    moving = (e) => {
+        const touch = e.targetTouches[0] || e.changedTouches[0];
+        this.moveX = touch.pageX;
+
+        if (Math.abs(this.moveX - this.startX) < 40) return;
+
+        this.refSliderDesktop.current.style.left = `${this.curLeft + 1 + this.moveX - this.startX}px`
+    }
+
+    endMove = (e) => {
+        this.getCurrentLeft();
+
+        if (Math.abs(this.moveX - this.startX) === 0 || (this.moveX === 0)) return;
+
+        const stayAtCur = Math.abs(this.moveX - this.startX) < 40 || this.moveX === 0 ? true : false;
+        const dir = this.startX < this.moveX ? 'left' : 'right';
+
+        if (!stayAtCur) {
+            dir === 'left' ? this.curSlide -= 1 : this.curSlide += 1;
+            if (this.curSlide < 0) {
+                this.curSlide = 0;
+            } else if (this.curSlide === this.totalSlides) {
+                this.curSlide -= 1;
+            }
+        }
+        this.gotoSlide();
+        this.restValues();
+    }
+
+    restValues = () => {
+        this.startX = 0;
+        this.moveX = 0;
     }
 
     render() {
         return (
             <div className={style.CarouselDesktop}>
-                {this.info.map((info, index) => {
-                    return (
-                        <div ref={this.refSlides[index]} key={index} className={style.PhotoDesktop}>
-                            <img data-src={info.image} alt={index} className={style.CarouselImage} />
-                            <Description key={index} item={info}></Description>
-                        </div>
-                    )
-                })}
-                <div className={style.ButtonsDesktop}>
-                <button className={`${style.Circle} ${style.Prev}`} onClick={(e) => { }}>{"<"}</button>
-                <button className={`${style.Circle} ${style.Next}`} onClick={(e) => { }}>{">"}</button>
+                <div ref={this.refSliderDesktop} onTouchStart={(e) => this.startMove(e)} onTouchMove={(e) => this.moving(e)} onTouchEnd={(e) => this.endMove(e)} className={style.SlideShowContainerDesktop}>
+                    {this.info.map((info, index) => {
+                        return (
+                            <div ref={this.refSlides[index]} key={index} className={style.PhotoDesktop}>
+                                <img data-src={info.image} alt={index} className={style.CarouselImage} />
+                                <Description key={index} item={info}></Description>
+                            </div>
+                        )
+                    })}
                 </div>
+                {this.totalSlides > 3 &&
+                    <div>
+                        <button className={`${style.Circle} ${style.Prev}`} onClick={(e) => { this.gotoSlide("prev") }}>{"<"}</button>
+                        <button className={`${style.Circle} ${style.Next}`} onClick={(e) => { this.gotoSlide("next") }}>{">"}</button>
+                    </div>
+                }
             </div >
-
         );
     }
 }
